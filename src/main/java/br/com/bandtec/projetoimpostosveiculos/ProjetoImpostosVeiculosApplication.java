054cb4e6283bc46124b2b870efca45518de1819f
João Marcos <joao.jesus@bandtec.com.br>
2020-09-04 18:43:55 -0300

    estrutura inicial do projeto

---
+++ src/main/java/br/com/bandtec/projetoimpostosveiculos/ProjetoImpostosVeiculosApplication.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.projetoimpostosveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoImpostosVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoImpostosVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bandtec/projetoimpostosveiculos/ProjetoImpostosVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.projetoimpostosveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoImpostosVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
